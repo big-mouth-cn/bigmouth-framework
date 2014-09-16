ef7b9eea7fde639399dabcb2957691682cdaaa1c
big-mouth-cn <huxiao.mail@qq.com>
2014-09-16 17:09:03 +0800

    添加获取主板序列号工具类

---
+++ src/main/java/org/bigmouth/framework/util/MiscUtils.java
@@ -0,0 +1,43 @@
+package org.bigmouth.framework.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+public final class MiscUtils {
+
+    private MiscUtils() {
+    }
+
+    public static String getMotherboardSN() {
+        String result = "";
+        try {
+            File file = File.createTempFile("realhowto", ".vbs");
+            file.deleteOnExit();
+            FileWriter fw = new java.io.FileWriter(file);
+            String vbs = "Set objWMIService = GetObject(\"winmgmts:\\\\.\\root\\cimv2\")\n"
+                    + "Set colItems = objWMIService.ExecQuery _ \n" + "   (\"Select * from Win32_BaseBoard\") \n"
+                    + "For Each objItem in colItems \n" + "    Wscript.Echo objItem.SerialNumber \n"
+                    + "    exit for  ' do the first cpu only! \n" + "Next \n";
+            fw.write(vbs);
+            fw.close();
+            Process p = Runtime.getRuntime().exec("cscript //NoLogo " + file.getPath());
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = input.readLine()) != null) {
+                result += line;
+            }
+            input.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result.trim();
+    }
+
+    public static void main(String[] args) {
+        String cpuId = MiscUtils.getMotherboardSN();
+        System.out.println(cpuId);
+    }
+}
